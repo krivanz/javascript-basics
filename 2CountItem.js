3716d3af357c7ba11dba53be1bb4c95d463cba96
Kriván Zoltán <krivan.zoltan@gmail.com>
2020-05-15 22:58:03 +0200

    2nd item Count

---
+++ 2CountItem.js
@@ -0,0 +1,12 @@
+// countItem
+const countElement = (arr, element) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log('Count 1:', countElement([1, 2, 1, 3, 1], 1));
